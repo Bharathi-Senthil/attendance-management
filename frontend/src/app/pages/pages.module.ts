92a49291b78e8958afbaecb9a8806f4e1ba96a22
Priyadharshan <priyadharshansenthil@gmail.com>
2023-02-11 17:25:57 +0530

    Update pages.module.ts

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { ComponentsModule } from "./../components/components.module";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
@@ -11,3 +12,22 @@ import { PagesRoutingModule } from "./pages.routing.module";
   imports: [CommonModule, PagesRoutingModule, RouterModule, ComponentsModule],
 })
 export class PagesModule {}
+=======
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { PagesRoutingModule } from './pages.routing.module';
+import { SidebarComponent } from '../components/sider/sider.component';
+import { AttendanceFromComponent } from '../components/attendance-from/attendance-from.component';
+@NgModule({
+    declarations: [BaseLayoutComponent,],
+    imports: [
+      CommonModule,
+      PagesRoutingModule,
+      RouterModule,
+      
+    ],
+  })
+  export class PagesModule {}
+>>>>>>> Stashed changes
