1711569cdbd5422bc2e06345b4de2d22f87110c7
Ramanan-Bala <58137577+Ramanan-Bala@users.noreply.github.com>
2023-02-11 17:13:00 +0530

    Update sider.component.ts

--- frontend/src/app/components/sider/sider.component.ts
+++ frontend/src/app/components/sider/sider.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { Menu } from 'src/app/Models/menu.module';
+import { Component } from "@angular/core";
+import { Menu } from "src/app/models/menu.module";
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sider.component.html',
-  styleUrls: ['./sider.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sider.component.html",
+  styleUrls: ["./sider.component.scss"],
 })
 export class SidebarComponent {
   menu: Menu[] = [
     {
-      title: 'Fill Attendance',
-      icon: 'checkBox',
+      title: "Fill Attendance",
+      icon: "checkBox",
     },
     {
-      title: 'Dashboard',
-      icon: 'team',
+      title: "Dashboard",
+      icon: "team",
     },
   ];
 }
