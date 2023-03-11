98be8010deef034d1845e4cc1db6835cadde29b0
BHARATHI <88837161+BHARATHI-GIT-HUB@users.noreply.github.com>
2023-03-11 12:31:06 +0530

    added chart

--- frontend/src/app/components/chart/chart.component.ts
+++ frontend/src/app/components/chart/chart.component.ts
@@ -17,6 +17,38 @@ import {
   styleUrls: ["./chart.component.scss"],
 })
 export class ChartComponent implements OnInit {
+  config = {
+    data: {
+      // values on X-Axis
+      labels: [
+        "2022-05-10",
+        "2022-05-11",
+        "2022-05-12",
+        "2022-05-13",
+        "2022-05-14",
+        "2022-05-15",
+        "2022-05-16",
+        "2022-05-17",
+      ],
+      datasets: [
+        {
+          label: "Present",
+          data: ["467", "576", "572", "79", "92", "574", "573", "576"],
+          backgroundColor: "#22c55e",
+          borderColor: "#22c55e",
+        },
+        {
+          label: "Absent",
+          data: ["542", "542", "536", "327", "17", "0.00", "538", "541"],
+          backgroundColor: "#ef4444",
+          borderColor: "#ef4444",
+        },
+      ],
+    },
+    options: {
+      aspectRatio: 2.5,
+    },
+  };
   constructor() {
     Chart.register(
       BarElement,
@@ -33,71 +65,26 @@ export class ChartComponent implements OnInit {
   ngOnInit(): void {
     new Chart("lineChart", {
       type: "line", //this denotes tha type of chart
-
-      data: {
-        // values on X-Axis
-        labels: [
-          "2022-05-10",
-          "2022-05-11",
-          "2022-05-12",
-          "2022-05-13",
-          "2022-05-14",
-          "2022-05-15",
-          "2022-05-16",
-          "2022-05-17",
-        ],
-        datasets: [
-          {
-            label: "Present",
-            data: ["467", "576", "572", "79", "92", "574", "573", "576"],
-            backgroundColor: "limegreen",
-            borderColor: "limegreen",
-          },
-          {
-            label: "Absent",
-            data: ["542", "542", "536", "327", "17", "0.00", "538", "541"],
-            backgroundColor: "red",
-            borderColor: "red",
-          },
-        ],
-      },
-      options: {
-        aspectRatio: 2.5,
-      },
+      ...this.config,
     });
 
-    new Chart("barchart", {
+    new Chart("barChart", {
       type: "bar", //this denotes tha type of chart
+      ...this.config,
+    });
 
+    new Chart("doughnut", {
+      type: "doughnut", //this denotes tha type of chart
       data: {
-        // values on X-Axis
-        labels: [
-          "2022-05-10",
-          "2022-05-11",
-          "2022-05-12",
-          "2022-05-13",
-          "2022-05-14",
-          "2022-05-15",
-          "2022-05-16",
-          "2022-05-17",
-        ],
+        labels: ["Present", "Absent", "OD"],
         datasets: [
           {
-            label: "Present",
-            data: ["467", "576", "572", "79", "92", "574", "573", "576"],
-            backgroundColor: "limegreen",
-            borderColor: "limegreen",
-          },
-          {
-            label: "Absent",
-            data: ["542", "542", "536", "327", "17", "0.00", "538", "541"],
-            backgroundColor: "red",
-            borderColor: "red",
+            label: "My First Dataset",
+            data: [300, 50, 100],
+            backgroundColor: ["#22c55e", "#ef4444", "#facc15"],
+            hoverOffset: 4,
           },
         ],
-      },
-      options: {
-        aspectRatio: 2.5,
       },
     });
   }
