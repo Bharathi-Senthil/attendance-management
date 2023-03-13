c81d88f42b94f0524236996056aa2341076164c8
Ramanan-Bala <58137577+Ramanan-Bala@users.noreply.github.com>
2023-03-13 09:12:32 +0530

    added centre text in chart

--- frontend/src/app/components/chart/chart.component.ts
+++ frontend/src/app/components/chart/chart.component.ts
@@ -90,6 +90,62 @@ export class ChartComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    let plugins = {
+      id: "doughnut",
+      afterDraw: (chart: any) => {
+        // Get ctx from string
+        var ctx = chart.ctx;
+
+        // Get options from the center object in options
+        // var centerConfig = chart.config.options.elements.center;
+        var fontStyle = "Arial";
+        var txt = this.date.toDateString();
+        var color = "#000";
+        var sidePadding = 20;
+        var sidePaddingCalculated =
+          (sidePadding / 100) * (chart.innerRadius * 2);
+        // Start with a base font of 30px
+        ctx.font = "20px " + fontStyle;
+
+        // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        var stringWidth = ctx.measureText(txt).width;
+        var elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
+
+        // Find out how much the font can grow in width.
+        var widthRatio = elementWidth / stringWidth;
+        var newFontSize = Math.floor(30 * widthRatio);
+        var elementHeight = chart.innerRadius * 2;
+
+        // Pick a new font size so it will not be larger than the height of label.
+        var fontSizeToUse = Math.min(newFontSize, elementHeight, 20);
+        var minFontSize = 20;
+        var wrapText = false;
+
+        if (minFontSize === undefined) {
+          minFontSize = 20;
+        }
+
+        if (minFontSize && fontSizeToUse < minFontSize) {
+          fontSizeToUse = minFontSize;
+          wrapText = true;
+        }
+
+        // Set font settings to draw it correctly.
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        var centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
+        var centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
+        ctx.font = fontSizeToUse + "px " + fontStyle;
+        ctx.fillStyle = color;
+
+        if (!wrapText) {
+          ctx.fillText(txt, centerX, centerY);
+          return;
+        }
+        ctx.fillText(txt, centerX, centerY);
+      },
+    };
+
     this.date = new Date();
 
     let addedDate = new Date();
@@ -97,6 +153,7 @@ export class ChartComponent implements OnInit {
     this.dateRange = [new Date(), addedDate];
 
     this.doughNut = new Chart("doughnut", {
+      plugins: [plugins],
       type: "doughnut",
       data: {
         labels: ["Absent", "Present"],
